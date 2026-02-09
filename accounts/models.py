14298a9980e084cb0a44592aac2a7072371b8f14
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-09 19:55:58 +0800

    feat: created a customer model, migrate and register the model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    date_created = models.DateField(auto_now_add=True, null=True)
