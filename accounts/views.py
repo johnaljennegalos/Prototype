aceb99d05725a411723bb8f16424070ae64145d3
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-10 20:26:39 +0800

    feat: display product list in template

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from accounts.models import Product
+
+
 # Create your views here.
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    ps = Product.objects.all()
+    return render(request, 'accounts/products.html', {'ps': ps})
 
 
 def customer(request):
