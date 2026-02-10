9fd83d36ec872b38cc37968257b33b0830f03d95
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-10 20:40:10 +0800

    feat: display neccessary data in dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from accounts.models import Product
+from accounts.models import Product, Order, Customer
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    context = {'orders': orders, 'customers': customers}
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
     ps = Product.objects.all()
