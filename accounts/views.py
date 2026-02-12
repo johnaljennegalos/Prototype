2a4eb101dc301e030fc8f079c9d84c1f3561a85b
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-12 21:50:18 +0800

    refactor: modify function to save POST data

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,12 @@ from .forms import OrderForm
 
 def registerPage(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
