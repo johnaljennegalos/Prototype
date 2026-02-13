6e3a9932656dece875e36cba60bc0bba257696fb
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-13 20:12:22 +0800

    feat: verify user login

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 from django.contrib import messages
 
@@ -30,6 +31,19 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect.')
+
     context = {}
     return render(request, 'accounts/login.html', context)
 
