768b72dc9202ff8dc18e260ef5589cfc4218e1d2
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-15 20:07:26 +0800

    feat: refactor the signals to receive the correct parameter

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,8 +9,8 @@ def customer_profile(sender, instance, created, **kwargs):
         instance.groups.add(group)
 
         Customer.objects.create(
-            user=user,
-            name=user.username,
+            user=instance,
+            name=instance.username,
         )
 
         print("customer created")
