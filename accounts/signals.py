bde2afb45c5040fd49751ad7ce60d83a0992cc95
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-15 20:06:45 +0800

    feat: refactor the signals to be able to query the group

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,9 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Customer
+from django.contrib.auth.models import Group
 
-def customer_profile(sender, instance, create, **kwargs):
+def customer_profile(sender, instance, created, **kwargs):
     if created:
         group = Group.objects.get(name='customer')
         instance.groups.add(group)
@@ -11,5 +12,7 @@ def customer_profile(sender, instance, create, **kwargs):
             user=user,
             name=user.username,
         )
+
+        print("customer created")
 
 post_save.connect(customer_profile, sender=User)
